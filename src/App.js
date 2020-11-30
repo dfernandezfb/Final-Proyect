62c20b0984c0dd1f5485fa4102f38039939e35ab
Diego Fernandez <dfernandezfb@gmail.com>
2020-11-30 01:10:08 -0300

    header sin funciones de buscador, admin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 //Components
 import EditCourse from './components/Modals/EditCourse.js';
+import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
 //Pages
 import AdminPage from "./pages/AdminPage";
 import LandingPage from "./pages/LandingPage";
@@ -10,17 +12,20 @@ import Home from "./pages/Home"
 import PrivateRoute from "./routes/PrivateRoute"
 
 function App() {
+  const dayHour = new Date().getHours();
   return (
   
     <>
     {/* <LandingPage /> */}
     <Router>
+      <Header dayHour={dayHour}/>
      <Switch>
        <Route exact path="/"  component={LandingPage}/> 
        <Route exact path="/courses/editar/:idC" component={EditCourse} />
        <Route exact path="/adminpage" component={AdminPage}/>
        <Route exact path="/home" component={Home} />
      </Switch>
+     {/* <Footer/> */}
     </Router>
   </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,50 @@ import {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import './Header.css'
+import '../../styles/colors-palette.css'
 import {Container,Col, Row } from 'react-bootstrap'
+import logo from "./../../images/logorecortadoclaro.png"
+import SearchBar from '../SearchBar/SearchBar'
+import UserMenu from '../UserMenu/UserMenu'
+const Header = ({dayHour}) => {
 
-const Header = () => {
-
-    return(
+    if(dayHour>=7 && dayHour<19)
+    {
+        var dayClassContainer= 'bg-color1 navbar-container';
+        var dayClassLink= 'navbar-link-claro color2';
+    }else
+    {
+        var dayClassContainer= 'bg-color4 navbar-container';
+        var dayClassLink= 'navbar-link-oscuro color2';
+    }
+return(
         <>  
-            <Container className="navbar-container">
+            <Container fluid className={dayClassContainer}>
                 <Row>
-                    <Col l={2}>LOGO</Col>
+                    <Col md={4}>
+                        <Navbar sticky="top" className="py-0">
+                            <Navbar.Brand href="/" className="py-0" >
+                                <img
+                                alt="Logo"
+                                src={logo}
+                                height="60"
+                                className="d-inline-block align-top"
+                                />{' '}
+                            </Navbar.Brand>
+                        </Navbar>
+                    </Col>
+                    <Col md={4} className="py-0">
+                        <SearchBar/>
+                    </Col>
+                    <Col md={4}>
+                        <UserMenu/>
+                    </Col>
                 </Row>
-                <Row>
-                    <Link className="navbar-link" to="/">Home</Link>
-                    <Link className="navbar-link" to="/">Favoritos</Link>
-                    <Link className="navbar-link" to="/">ALGO</Link>
-                    <Link className="navbar-link" to="/">ALGO MAS</Link>
+                <Row> {/*usar new Date().getHours() para tema oscuro*/}
+                    <Link className={dayClassLink} to="/">Home</Link>
+                    <Link className={dayClassLink} to="/">Categorías</Link>
+                    <Link className={dayClassLink} to="/">Ayuda</Link>
+                    <Link className={dayClassLink} to="/">Algo mas</Link>
                 </Row>
             </Container>
         </>
