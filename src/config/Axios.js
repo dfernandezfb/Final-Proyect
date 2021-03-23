2771aac57a194f099f54b197ea111b9887c22be7
Diego Fernandez <diego@pixeladas.es>
2021-03-23 15:56:30 -0300

    deploy test 1

--- src/config/Axios.js
+++ src/config/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-  baseURL: 'http://localhost:4000/api'
+  baseURL: 'https://knowledgeacademy-back.herokuapp.com/api'
 })
 
 export default clientAxios;
